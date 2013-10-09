7530657942eca6d81d1948030055fe4857ac893c
David Gero <davidpgero@gmail.com>
2013-10-09 22:01:03 +0200

    Add Time.now as default paramterers

--- lib/client.rb
+++ lib/client.rb
@@ -4,16 +4,14 @@ module BusinessHourCalculator
 
       attr_reader :date, :estimate_hour
 
-    	def initialize(estimate_hour, date)
-    		@date = date || Time.now()
+    	def initialize(estimate_hour, date = Time.now())
+    		@date = date
     		@estimate_hour = estimate_hour
     	end
 
     	def calculated_due_date
     		time_format(calculate_due_date)
     	end
-
-      private
 
     	def calculate_due_date
     		estimate_hour.business_hour.after(date)
