d459fe8155b0bec2206738c1bd16eb9d374ebfe2
David Gerő <davidpgero@gmail.com>
2013-11-11 11:26:33 +0100

    reformat

--- lib/client.rb
+++ lib/client.rb
@@ -1,20 +1,20 @@
 module BusinessHourCalculator
-    class Client
-    	include TimeFormat
+  class Client
+    include TimeFormat
 
-      attr_reader :date, :estimate_hour
+    attr_reader :date, :estimate_hour
 
-    	def initialize(estimate_hour, date = Time.now())
-    		@date = date
-    		@estimate_hour = estimate_hour
-    	end
-
-    	def calculated_due_date
-    		time_format(calculate_due_date)
-    	end
-
-    	def calculate_due_date
-    		estimate_hour.business_hour.after(date)
-    	end
+    def initialize(estimate_hour, date = Time.now())
+    	@date = date
+    	@estimate_hour = estimate_hour
     end
+
+    def calculated_due_date
+    	time_format(calculate_due_date)
+    end
+
+    def calculate_due_date
+    	estimate_hour.business_hour.after(date)
+    end
+  end
 end # BusinessHourCalculator
